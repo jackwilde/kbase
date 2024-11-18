f05d105b4ed1c57ebee4dc8d21256d59e3b59ca0
Jack Wilde <jack.wilde@uwe.ac.uk>
2024-11-18 21:06:36 +0000

    update styles

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.AccountView.as_view(), name='my-account'),
+    path('reset-password/', views.ResetPasswordView.as_view(), name='reset-password'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,18 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, TemplateView
+from authentication.models import User
 
 
 # Create your views here.
-class AccountView(TemplateView):
+class AccountView(UpdateView):
     template_name = 'account/account.html'
+    model = User
+    fields = ["first_name", "last_name", "email"]
+    success_url = reverse_lazy('dashboard')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ResetPasswordView(TemplateView):
+    template_name = 'account/reset-password.html'
