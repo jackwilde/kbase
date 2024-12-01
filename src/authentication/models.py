56c01e816dfd70ee2eefd511e26cb9ff5f69da28
Jack Wilde <jack.wilde@uwe.ac.uk>
2024-12-01 15:00:02 +0000

    Create authentication model tests

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError, PermissionDenied
-from django.db import models
+from django.core.validators import RegexValidator
+from django.db import models, IntegrityError
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
@@ -15,32 +16,18 @@ class CustomUserManager(BaseUserManager):
         :return: user object
         """
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValidationError("Users must have an email address")
         if not first_name:
-            raise ValueError("Users must have a first name")
+            raise ValidationError("Users must have a first name")
         if not last_name:
-            raise ValueError("Users must have a last name")
+            raise ValidationError("Users must have a last name")
 
         email = self.normalize_email(email)
         user = self.model(email=email, first_name=first_name, last_name=last_name)
 
+
         user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-
-    def create_superuser(self, email, first_name, last_name, password=None):
-        """
-        Creates and saves a new superuser
-        :param email: Email address
-        :param first_name: First Name
-        :param last_name: Last Name
-        :param password: Password
-        :return: user object
-        """
-        user = self.create_user(email=email, first_name=first_name, last_name=last_name, password=password)
-        user.is_staff = True
-        user.is_superuser = True
+        user.full_clean()
         user.save(using=self._db)
         return user
 
@@ -49,8 +36,12 @@ class User(AbstractBaseUser):
     username = None
     email = models.EmailField(unique=True, null=False, blank=False)
     is_admin = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=50, null=False, blank=False)
-    last_name = models.CharField(max_length=50, null=False, blank=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False, validators=[
+        RegexValidator(regex='^[A-Za-z]+$', message='First name can only contain letters.'),
+    ])
+    last_name = models.CharField(max_length=50, null=False, blank=False, validators=[
+        RegexValidator(regex='^[A-Za-z]+$', message='Last name can only contain letters.')
+    ])
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
@@ -81,7 +72,10 @@ class Group(models.Model):
         if self.id == 1:
             raise PermissionDenied("The 'All Users' group cannot be altered.")
         self.name = self.name.lower()
-        super().save(*args, **kwargs)
+        try:
+            super().save(*args, **kwargs)
+        except IntegrityError:
+            raise ValidationError("A group with that name already exists.")
 
 
     def delete(self, *args, **kwargs):
--- src/authentication/tests/test_models.py
+++ src/authentication/tests/test_models.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-
-from django_knowledgebase.settings import SECRET_KEY
 from authentication.models import User, Group
 from django.core.exceptions import ValidationError, PermissionDenied
 
@@ -8,68 +6,165 @@ from django.core.exceptions import ValidationError, PermissionDenied
 class UserModelTestCase(TestCase):
     def setUp(self):
         # Get all users group for testing
-        self.all_users_group = Group.objects.get(name="all users")
+        self.all_users_group = Group.objects.get(name='all users')
 
     def test_create_user(self):
         # Test creating a user
         user = User.objects.create_user(
-            email="testuser@example.com",
-            first_name="Test",
-            last_name="User",
-            password="securepassword123"
+            email='testuser@example.com',
+            first_name='Test',
+            last_name='User',
+            password='djangopassword123'
         )
-        self.assertEqual(user.email, "testuser@example.com")
-        self.assertEqual(user.full_name, "Test User")
-        self.assertTrue(user.check_password("securepassword123"))
+        self.assertEqual(user.email, 'testuser@example.com')
+        self.assertEqual(user.full_name, 'Test User')
+        self.assertTrue(user.check_password('djangopassword123'))
         self.assertIn(self.all_users_group, user.groups.all())
         self.assertEqual(user.is_admin, False)
 
+    def test_delete_user(self):
+        # Test deleting a user
+        user = User.objects.create_user(
+            email='testuser@example.com',
+            first_name='Test',
+            last_name='User',
+            password='djangopassword123'
+        )
+        self.assertTrue(User.objects.filter(email='testuser@example.com').exists())
+        user.delete()
+        self.assertFalse(User.objects.filter(email='testuser@example.com').exists())
 
-    # def test_create_user_without_email(self):
-    #     # Test user creation without an email should raise ValueError
-    #     with self.assertRaises(ValueError):
-    #         User.objects.create_user(
-    #             email="",
-    #             first_name="Test",
-    #             last_name="User",
-    #             password="securepassword123"
-    #         )
-    #
-    # def test_create_user_without_first_name(self):
-    #     # Test user creation without first name should raise ValueError
-    #     with self.assertRaises(ValueError):
-    #         User.objects.create_user(
-    #             email="testuser@example.com",
-    #             first_name="",
-    #             last_name="User",
-    #             password="securepassword123"
-    #         )
-    #
-    # def test_create_user_without_last_name(self):
-    #     # Test user creation without last name should raise ValueError
-    #     with self.assertRaises(ValueError):
-    #         User.objects.create_user(
-    #             email="testuser@example.com",
-    #             first_name="Test",
-    #             last_name="",
-    #             password="securepassword123"
-    #         )
-    #
-    # def test_user_str_method(self):
-    #     # Test string representation of the user
-    #     user = User.objects.create_user(
-    #         email="testuser@example.com",
-    #         first_name="Test",
-    #         last_name="User",
-    #         password="securepassword123"
-    #     )
-    #     self.assertEqual(str(user), "testuser@example.com")
-    #
-    # def test_all_users_group_protection(self):
-    #     # Test that "All Users" group cannot be edited or deleted
-    #     with self.assertRaises(PermissionDenied):
-    #         self.all_users_group.delete()
-    #
-    #     with self.assertRaises(PermissionDenied):
-    #         self.all_users_group.name = "New Name"
-    #         self.all_users_group.save()
+    def test_create_user_without_email(self):
+        # Test creating a user without email raises ValidationError
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='',
+                first_name='Test',
+                last_name='User',
+                password='djangopassword123'
+            )
+
+    def test_create_user_without_first_name(self):
+        # Test creating a user without first name raises ValidationError
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='testuser@example.com',
+                first_name='',
+                last_name='User',
+                password='djangopassword123'
+            )
+
+    def test_create_user_without_last_name(self):
+        # Test creating a user without last name raises ValidationError
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='testuser@example.com',
+                first_name='Test',
+                last_name='',
+                password='djangopassword123'
+            )
+
+    def test_create_user_with_duplicate_email(self):
+        # Test creating a user with duplicate email raises ValidationError
+        User.objects.create_user(
+            email='testuser@example.com',
+            first_name='Test',
+            last_name='User',
+            password='djangopassword123'
+        )
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='testuser@example.com',
+                first_name='Second',
+                last_name='User',
+                password='djangopassword321'
+            )
+
+    def test_create_user_with_invalid_first_name(self):
+        # Test creating a user with invalid first name raises ValidationError
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='testuser@example.com',
+                first_name='T3<5T',
+                last_name='User',
+                password='djangopassword123'
+            )
+
+    def test_create_user_with_invalid_last_name(self):
+        # Test creating a user with invalid first name raises ValidationError
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='testuser@example.com',
+                first_name='Test',
+                last_name='U5€R',
+                password='djangopassword123'
+            )
+
+    def test_create_user_with_invalid_email(self):
+        # Test creating a user with invalid email raises ValidationError
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email='testuser@example.com',
+                first_name='Test',
+                last_name='U5€R',
+                password='djangopassword123'
+            )
+
+    def test_user_str_method(self):
+        # Test object string
+        user = User.objects.create_user(
+            email='testuser@example.com',
+            first_name='Test',
+            last_name='User',
+            password='djangopassword123'
+        )
+        self.assertEqual(str(user), 'testuser@example.com')
+
+
+class GroupModelTestCase(TestCase):
+    def setUp(self):
+        # Create users to test with
+        self.user1 = User.objects.create_user(
+            email='testuser@example.com',
+            first_name='Test',
+            last_name='User',
+            password='djangopassword123'
+        )
+        self.user2 = User.objects.create_user(
+            email='seconduser@example.com',
+            first_name='Second',
+            last_name='User',
+            password='djangopassword123'
+        )
+
+    def test_all_users_group_exists(self):
+        # Test that 'All Users' group has been created
+        group = Group.objects.get(name='all users')
+        self.assertIsNotNone(group)
+
+        # Test that it has the correct pk and name
+        self.assertEqual(group.pk, 1)
+        self.assertEqual(group.name, 'all users')
+
+    def test_all_users_group_is_protected(self):
+        group = Group.objects.get(name='all users')
+        # Test that 'All Users' group cannot be renamed or deleted
+        with self.assertRaises(PermissionDenied):
+            group.delete()
+
+        with self.assertRaises(PermissionDenied):
+            group.name = 'New Name'
+            group.save()
+
+    def test_create_group(self):
+        group = Group.objects.create(name='new group')
+        group.users.add(self.user1, self.user2)
+        self.assertEqual(group.name, 'new group')
+        self.assertEqual(group.users.count(), 2)
+        self.assertIn(self.user1, group.users.all())
+        self.assertIn(self.user2, group.users.all())
+
+    def test_create_group_with_duplicate_name(self):
+        Group.objects.create(name='new group')
+        with self.assertRaises(ValidationError):
+            Group.objects.create(name='new group')
