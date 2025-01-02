8576f398150912bb1c943eb66887439f6a04d595
Jack Wilde <jack.wilde@uwe.ac.uk>
2025-01-02 09:49:46 +0000

    Use date_joined for token generation

--- src/authentication/utils.py
+++ src/authentication/utils.py
@@ -25,6 +25,21 @@ class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
         except (DjangoUnicodeDecodeError, ValueError):
             raise InvalidTokenError("The token could not be decoded properly or is in an invalid format.")
 
+    def _make_hash_value(self, user, timestamp):
+        """
+        Customised method to use date joined instead of last_login
+        """
+        # Truncate microseconds so that tokens are consistent even if the
+        # database doesn't support microseconds.
+        joined_timestamp = (
+            ""
+            if user.date_joined is None
+            else user.date_joined.replace(microsecond=0, tzinfo=None)
+        )
+        email_field = user.get_email_field_name()
+        email = getattr(user, email_field, "") or ""
+        return f"{user.pk}{user.password}{joined_timestamp}{timestamp}{email}"
+
 
 class InvalidTokenError(Exception):
     """
