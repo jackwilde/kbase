03a1caf4f18a89b0a659b95a01475cddb3366020
Jack Wilde <jack.wilde@uwe.ac.uk>
2025-01-02 09:49:10 +0000

    Update verified user checks

--- src/django_knowledgebase/middleware.py
+++ src/django_knowledgebase/middleware.py
@@ -1,24 +1,34 @@
 from django.urls import reverse, resolve
 from django.shortcuts import redirect
 from django.contrib import messages
-from django.contrib.auth import logout
 from django.utils.deprecation import MiddlewareMixin
 
 class VerifiedUserMiddleware(MiddlewareMixin):
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        # Do nothing to verified authenticated users
-        if request.user.is_authenticated and request.user.is_verified:
-            return None
+    """
+    Custom middleware to redirect unverified users when they log in.
+    This will prevent them from accessing anything inside the application until they are verified.
+    """
+    def __init__(self, get_response=None):
+        super().__init__(get_response)
+        # Views that are allowed without verification
+        self.allowed_views = (
+            'verify-email',
+            'sign-out',
+            're-verify'
+        )
 
+    def process_view(self, request):
+        """
+        Checks if the user is verified and redirects them to the re-verification page if they are not.
+        """
         # Do nothing to unauthenticated users
         if not request.user.is_authenticated:
             return None
-
-        # Sign out and redirect unverified users who try and sign in
-        if request.user.is_authenticated and not request.user.is_verified:
-            logout(request)
-            messages.warning(request, 'Your account is not verified and has been signed out. '
+        # Do nothing to verified users
+        elif request.user.is_verified:
+            return None
+        # Redirect unverified users to the reverify page
+        elif resolve(request.path).view_name not in self.allowed_views:
+            messages.warning(request, 'Your account is not verified. '
                                       'Check your email or request a new verification link.')
             return redirect(reverse('re-verify'))
-
-        return None
