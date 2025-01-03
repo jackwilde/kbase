8920ce1b359bffcfbb604eea4e9fb4a67984539e
Jack Wilde <jack.wilde@uwe.ac.uk>
2025-01-03 10:54:51 +0000

    Setup account messages

--- src/account/views.py
+++ src/account/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import UpdateView, TemplateView
 from django.contrib.auth.views import PasswordChangeView
 from authentication.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,11 +10,15 @@ class AccountView(UpdateView):
     template_name = 'account/account.html'
     model = User
     fields = ["first_name", "last_name", "email"]
-    success_url = reverse_lazy('dashboard')
+    success_url = reverse_lazy('my-account')
 
     # Set object as the logged-in user
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_success_url(self):
+        messages.success(self.request, "Account details updated successfully.")
+        return super().get_success_url()
 
 
 class ResetPasswordView(PasswordChangeView):
@@ -23,3 +28,7 @@ class ResetPasswordView(PasswordChangeView):
     # Set object as the logged-in user
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_success_url(self):
+        messages.success(self.request, "Password changed successfully.")
+        return super().get_success_url()
